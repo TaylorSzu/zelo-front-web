5fdfc302e606e9168306f352fc2cda964f42a7e2
IsraelBryan <umbreongame0@gmail.com>
2025-05-15 11:10:53 -0300

    Feat: Fix Bugs

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['zelloapp.com.br']
+    allowedHosts: ['zelloapp.com.br'],
+    allowedHosts: ['localhost'],
   }
 })
