562242f190bc17fe844554e67fe25dcf3c90429f
IsraelBryan <umbreongame0@gmail.com>
2025-05-16 10:25:56 -0300

    Feat: Cambirria

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     https: {
-      key: '149092ff7467f365b8d8c68b7e090668af6c41e1ea07da4f1f12d1d075793671',
-      cert:'19708bd61cbff47fbd7aeaf7f4e8f5d12c488f508b1d956ebd6123bb3dcbe7d4',
+      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'zelloapp.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'zelloapp.pem')),
     },
     proxy: {
       '/usuario': {
