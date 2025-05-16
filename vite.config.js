3839342dc64d2d1c78173de4a61239e345350e37
IsraelBryan <umbreongame0@gmail.com>
2025-05-16 09:23:52 -0300

    Feat: Tome David

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
     allowedHosts: ['zelloapp.com.br'],
     host: '0.0.0.0',
     port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'zelloapp.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'zelloapp.pem')),
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'certs', 'zelloapp.pem')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'zelloapp.pem')),
+    // },
     proxy: {
       '/usuario': {
         target: 'http://localhost:5171',
