068b446e98b85fb7c80b181f16ebad87917ad201
IsraelBryan <umbreongame0@gmail.com>
2025-05-16 08:34:53 -0300

    Feat: Fixing AllowedHosts

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-
+    allowedHosts: ['zelloapp.com.br'],
     host: '0.0.0.0',
     port: 5173,
     proxy: {
