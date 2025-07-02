b17c8b8f9e5f9ca48d6d4bba87cdbf666f6641e0
IsraelBryan <umbreongame0@gmail.com>
2025-07-02 17:18:31 -0300

    server

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import fs from 'fs';
-import path from 'path';
-// https://vite.dev/config/
 
 //DAVID NÃO MEXA NESTE ARQUIVO
 export default defineConfig({
     plugins: [react()],
     server: {
-    allowedHosts: ['zelloapp.com.br'],
-    host: '0.0.0.0',
-    port: 5173,
-    https: {
-     cert: fs.readFileSync('/home/zelloapp/.ssl/fullchain.pem'),
-      key: fs.readFileSync('/home/zelloapp/.ssl/privkey.pem'),
+        allowedHosts: ['zelloapp.com.br'],
+        host: '0.0.0.0',
+        port: 5173,
+        https: {
+            cert: fs.readFileSync('/home/zelloapp/.ssl/fullchain.pem'),
+            key: fs.readFileSync('/home/zelloapp/.ssl/privkey.pem'),
+        },
+        proxy: {
+            '/usuario': {
+                target: 'https://www.zelloapp.com.br:5171/',
+                changeOrigin: true,
+                secure: false, // Se o backend usa certificado autoassinado
+            },
+        },
+        hmr: {
+            protocol: 'wss',
+            host: 'zelloapp.com.br',
+            port: 5173,
+        },
     },
-    proxy: {
-      '/usuario': {
-        target: 'http://localhost:5171/',
-        changeOrigin: true,
-      },
-    hmr: {
-      protocol: 'wss',
-      host: 'zelloapp.com.br',
-      port: 5173,
-    }
-  }
-  },
 });
